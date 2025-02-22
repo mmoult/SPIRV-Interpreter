89a2d453baa67d809055f787b8a1c943e273e121
mmoult <30711895+mmoult@users.noreply.github.com>
2025-02-22 14:21:12 -0600

    Set up GitHub actions
    It may need some adjusting.

--- test/example-runner.py
+++ test/example-runner.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # © SPIRV-Interpreter @ https://github.com/mmoult/SPIRV-Interpreter
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
